bfc58adec71bd8aacb13b53f2a6a0f6879ddc574
ysk125103 <yashmeet.singh@outlook.com>
2024-01-21 10:20:33 -0500

    Reuse Greetings component

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@ function App() {
   return (
     <>
       <h1>My First React App</h1>
-      {/* Add Greetings component & 
-      pass visitor's name as a prop */}
+      {/* Add Greetings component &
+       pass visitor's name as a prop */}
       <Greetings name="Monica" />
+      {/* Reuse Greetings component to 
+      welcome the second visitor */}
+      <Greetings name="Sunny" />
     </>
   );
 }
