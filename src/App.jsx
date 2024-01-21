cf77a30ed0d7f4ca20ccb277dd2711f8ca5710bd
ysk125103 <yashmeet.singh@outlook.com>
2024-01-21 10:09:10 -0500

    JSX comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Greetings from "./Greetings.jsx";
 
 function App() {
   return (
-    // Add Greetings component &
-    // pass visitor's name as a prop
     <>
       <h1>My First React App</h1>
+      {/* Add Greetings component & 
+      pass visitor's name as a prop */}
       <Greetings name="Monica" />
     </>
   );
